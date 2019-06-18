eeebfbe51890c1a5d28e0368f394124db7b0044b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-18 08:34:30 +0530

    adding SMDNode class

--- squaremiddistortview/src/main/java/com/anwesh/uiprojects/squaremiddistortview/SquareMidDistortView.kt
+++ squaremiddistortview/src/main/java/com/anwesh/uiprojects/squaremiddistortview/SquareMidDistortView.kt
@@ -136,8 +136,52 @@ class SquareMidDistortedView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class SMDNode(var i : Int, val state : State = State()) {
+
+        private var next : SMDNode? = null
+        private var prev : SMDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SMDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSMDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SMDNode {
+            var curr : SMDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
