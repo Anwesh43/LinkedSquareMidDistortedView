3efbaad0325ddc91827645a71bcaf2a3f5919b19
Anwesh43 <anweshthecool0@gmail.com>
2019-06-18 08:14:17 +0530

    adding libraries and constants

--- squaremiddistortview/src/main/java/com/anwesh/uiprojects/squaremiddistortview/SquareMidDistortView.kt
+++ squaremiddistortview/src/main/java/com/anwesh/uiprojects/squaremiddistortview/SquareMidDistortView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.squaremiddistortview
 /**
  * Created by anweshmishra on 18/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val offsetFactor : Float = 0.25f
