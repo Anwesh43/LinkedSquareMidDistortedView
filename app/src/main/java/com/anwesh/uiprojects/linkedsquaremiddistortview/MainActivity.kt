1e5f19e0c98f5b8742bf655cc057a0a43303aec8
Anwesh43 <anweshthecool0@gmail.com>
2019-06-18 08:43:52 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaremiddistortview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaremiddistortview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsquaremiddistortview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.squaremiddistortview.SquareMidDistortedView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SquareMidDistortedView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
