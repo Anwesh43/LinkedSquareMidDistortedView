d2868c569fad89f34ceceec00b970d0aa9f674ee
Anwesh43 <anweshthecool0@gmail.com>
2019-06-18 08:43:03 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedsquaremiddistortview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquaremiddistortview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedsquaremiddistortview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.squaremiddistortview.SquareMidDistortedView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareMidDistortedView.create(this)
     }
 }
